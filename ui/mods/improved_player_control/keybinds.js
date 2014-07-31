051b02099b2155e97100c9d078a3394a25e998e0
Justin Love <git@JustinLove.name>
2014-07-31 13:16:20 -0500

    leftover debugging

--- ui/mods/improved_player_control/keybinds.js
+++ ui/mods/improved_player_control/keybinds.js
@@ -1,7 +1,6 @@
 (function() {
   var controlPlayer = function(n) {
     return function() {
-      console.log(n, api.panels.devmode)
       api.panels && api.panels.devmode && api.panels.devmode.message('improved_player_control_change', n)
     }
   }
--- ui/mods/improved_player_control/live_game.js
+++ ui/mods/improved_player_control/live_game.js
@@ -9,7 +9,6 @@ model.playerControlFlags.subscribe(function(flags) {
     var border = 'rgb(' + Math.floor(player.secondary_color[0]) + ',' + Math.floor(player.secondary_color[1]) + ',' + Math.floor(player.secondary_color[2]) + ')';
 
     var style = {'background-color': color, 'border-color': border}
-    console.log(style, api.panels.devmode, api.panels.sandbox)
 
     api.panels.devmode && api.panels.devmode.message('improved_player_control_colors', style)
     api.panels.sandbox && api.panels.sandbox.message('improved_player_control_colors', style)
