dbc3e32dbc9fa58575bd6cbbf7ce04bc6183c80c
Justin Love <git@JustinLove.name>
2014-07-31 11:30:05 -0500

    set border to secondary color

--- ui/mods/improved_player_control/live_game_devmode.js
+++ ui/mods/improved_player_control/live_game_devmode.js
@@ -7,7 +7,10 @@
 
     if (player) {
       var color = 'rgba(' + Math.floor(player.primary_color[0]) + ',' + Math.floor(player.primary_color[1]) + ',' + Math.floor(player.primary_color[2]) + ', 0.5)';
+      var border = 'rgb(' + Math.floor(player.secondary_color[0]) + ',' + Math.floor(player.secondary_color[1]) + ',' + Math.floor(player.secondary_color[2]) + ')';
+      $('.container').css({'background-color': color, 'border-color': border})
 
+      var color = 'rgba(' + Math.floor(player.primary_color[0]) + ',' + Math.floor(player.primary_color[1]) + ',' + Math.floor(player.primary_color[2]) + ', 0.5)';
       $('.container').css('background-color', color)
     }
 
