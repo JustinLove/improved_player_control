594d271283af0aa4157d15534462e709b9a4e15f
Justin Love <git@JustinLove.name>
2014-07-31 11:27:36 -0500

    set background color to player color

--- ui/mods/improved_player_control/live_game_devmode.js
+++ ui/mods/improved_player_control/live_game_devmode.js
@@ -1,5 +1,18 @@
 (function() {
   // current state appears to be pre-ko-foreach
 
+  model.currentPlayer = ko.computed(function() {
+    var index = model.playerControlFlags().indexOf(true)
+    var player = model.players()[index]
+
+    if (player) {
+      var color = 'rgba(' + Math.floor(player.primary_color[0]) + ',' + Math.floor(player.primary_color[1]) + ',' + Math.floor(player.primary_color[2]) + ', 0.5)';
+
+      $('.container').css('background-color', color)
+    }
+
+    return player
+  })
+
   $('tr:nth-child(2)').attr('data-bind', 'click: function () { $parent.updatePlayerControlFlag($index()); }')
 })()
